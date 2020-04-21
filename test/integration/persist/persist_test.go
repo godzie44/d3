70b692c463cdcf0169c21933d677935abd606264
godzie44 <godzie@yandex.ru>
2020-04-22 01:22:02 +0300

    persist test code style fixes

--- test/integration/persist/persist_test.go
+++ test/integration/persist/persist_test.go
@@ -54,33 +54,33 @@ func (o *PersistsTS) SetupSuite() {
 		profile_id integer,
 		name character varying(200) NOT NULL
 	)`)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
 	_, err = o.pgDb.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS profile_p(
 		id SERIAL PRIMARY KEY,
 		shop_id integer, --for test circular ref
 		description character varying(1000) NOT NULL
 	)`)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
 	_, err = o.pgDb.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS book_p(
 		id SERIAL PRIMARY KEY,
 		name text NOT NULL,
 		shop_id integer
 	)`)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
 	_, err = o.pgDb.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS author_p(
 		id SERIAL PRIMARY KEY,
 		name character varying(200) NOT NULL
 	)`)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
 	_, err = o.pgDb.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS book_author_p(
 		book_id integer NOT NULL,
 		author_id integer NOT NULL
 	)`)
-	o.Assert().NoError(err)
+	o.NoError(err)
 }
 
 func (o *PersistsTS) TearDownSuite() {
@@ -91,7 +91,7 @@ DROP TABLE book_author_p;
 DROP TABLE shop_p;
 DROP TABLE profile_p;
 `)
-	o.Assert().NoError(err)
+	o.NoError(err)
 }
 
 func (o *PersistsTS) TearDownTest() {
@@ -102,7 +102,7 @@ delete from book_author_p;
 delete from shop_p;
 delete from profile_p;
 `)
-	o.Assert().NoError(err)
+	o.NoError(err)
 }
 
 func TestPersistsSuite(t *testing.T) {
@@ -111,11 +111,11 @@ func TestPersistsSuite(t *testing.T) {
 
 func (o *PersistsTS) TestSimpleInsert() {
 	d3Orm := orm.NewOrm(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 	repository, err := d3Orm.CreateRepository(session, (*Shop)(nil))
-	o.Assert().NoError(err)
+	o.NoError(err)
 
 	shop := &Shop{
 		Books: nil,
@@ -125,11 +125,11 @@ func (o *PersistsTS) TestSimpleInsert() {
 		Name: "simple-shop",
 	}
 
-	o.Assert().NoError(repository.Persists(shop))
-	o.Assert().NoError(session.Flush())
+	o.NoError(repository.Persists(shop))
+	o.NoError(session.Flush())
 
-	o.Assert().NotEqual(0, shop.Id.Int32)
-	o.Assert().NotEqual(0, shop.Profile.Unwrap().(*ShopProfile).Id.Int32)
+	o.NotEqual(0, shop.Id.Int32)
+	o.NotEqual(0, shop.Profile.Unwrap().(*ShopProfile).Id.Int32)
 
 	helpers.NewPgTester(o.T(), o.pgDb).
 		SeeOne("SELECT * FROM shop_p WHERE name='simple-shop' AND profile_id IS NOT NULL").
@@ -138,20 +138,20 @@ func (o *PersistsTS) TestSimpleInsert() {
 
 func (o *PersistsTS) TestBigInsert() {
 	d3Orm := orm.NewOrm(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 	shop, err := createAndPersistsShop(d3Orm, session)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
-	o.Assert().NotEqual(0, shop.Id.Int32)
-	o.Assert().NotEqual(0, shop.Profile.Unwrap().(*ShopProfile).Id.Int32)
-	o.Assert().NotEqual(0, shop.Books.Get(0).(*Book).Id.Int32)
-	o.Assert().NotEqual(0, shop.Books.Get(1).(*Book).Id.Int32)
-	o.Assert().NotEqual(0, shop.Books.Get(0).(*Book).Authors.Get(0).(*Author).Id.Int32)
-	o.Assert().NotEqual(0, shop.Books.Get(1).(*Book).Authors.Get(0).(*Author).Id.Int32)
+	o.NotEqual(0, shop.Id.Int32)
+	o.NotEqual(0, shop.Profile.Unwrap().(*ShopProfile).Id.Int32)
+	o.NotEqual(0, shop.Books.Get(0).(*Book).Id.Int32)
+	o.NotEqual(0, shop.Books.Get(1).(*Book).Id.Int32)
+	o.NotEqual(0, shop.Books.Get(0).(*Book).Authors.Get(0).(*Author).Id.Int32)
+	o.NotEqual(0, shop.Books.Get(1).(*Book).Authors.Get(0).(*Author).Id.Int32)
 
 	helpers.NewPgTester(o.T(), o.pgDb).
 		SeeOne("SELECT * FROM shop_p WHERE name='shop' AND profile_id IS NOT NULL").
@@ -164,104 +164,101 @@ func (o *PersistsTS) TestBigInsert() {
 func (o *PersistsTS) TestNoNewQueriesIfDoubleFlush() {
 	dbAdapter := helpers.NewDbAdapterWithQueryCounter(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
 	d3Orm := orm.NewOrm(dbAdapter)
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 
 	_, err := createAndPersistsShop(d3Orm, session)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 	insertCounter, updCounter := dbAdapter.InsertCounter(), dbAdapter.UpdateCounter()
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
-	o.Assert().Equal(insertCounter, dbAdapter.InsertCounter())
-	o.Assert().Equal(updCounter, dbAdapter.UpdateCounter())
+	o.Equal(insertCounter, dbAdapter.InsertCounter())
+	o.Equal(updCounter, dbAdapter.UpdateCounter())
 }
 
 func (o *PersistsTS) TestInsertThenUpdate() {
 	dbAdapter := helpers.NewDbAdapterWithQueryCounter(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
 	d3Orm := orm.NewOrm(dbAdapter)
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 
 	shop, err := createAndPersistsShop(d3Orm, session)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
 	shop.Name = "new shop"
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
+	o.Equal(1, dbAdapter.UpdateCounter())
 	helpers.NewPgTester(o.T(), o.pgDb).
 		SeeOne("SELECT * FROM shop_p WHERE name='new shop'").
 		See(0, "SELECT * FROM shop_p WHERE name='shop'")
-
-	o.Assert().Equal(1, dbAdapter.UpdateCounter())
 }
 
 func (o *PersistsTS) TestInsertThenUpdateOToMRelation() {
 	dbAdapter := helpers.NewDbAdapterWithQueryCounter(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
 	d3Orm := orm.NewOrm(dbAdapter)
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 
 	shop, err := createAndPersistsShop(d3Orm, session)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
 	shop.Books.Remove(0)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
+	o.Equal(1, dbAdapter.UpdateCounter())
 	helpers.NewPgTester(o.T(), o.pgDb).
 		SeeOne("SELECT * FROM book_p WHERE shop_id IS NOT NULL").
 		SeeOne("SELECT * FROM book_p WHERE shop_id IS NULL")
-
-	o.Assert().Equal(1, dbAdapter.UpdateCounter())
 }
 
 func (o *PersistsTS) TestInsertThenUpdateMToMRelations() {
 	dbAdapter := helpers.NewDbAdapterWithQueryCounter(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
 	d3Orm := orm.NewOrm(dbAdapter)
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 
 	shop, err := createAndPersistsShop(d3Orm, session)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
 	book := shop.Books.Get(0).(*Book)
 	author := book.Authors.Get(1).(*Author)
 
 	book.Authors.Remove(1)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
+	o.Equal(1, dbAdapter.DeleteCounter())
 	helpers.NewPgTester(o.T(), o.pgDb).
 		See(0, "SELECT * FROM book_author_p WHERE book_id = $1 and author_id = $2", book.Id, author.Id)
-
-	o.Assert().Equal(1, dbAdapter.DeleteCounter())
 }
 
 func (o *PersistsTS) TestInsertThenFullUpdate() {
 	dbAdapter := helpers.NewDbAdapterWithQueryCounter(adapter.NewGoPgXAdapter(o.pgDb, &adapter.SquirrelAdapter{}))
 	d3Orm := orm.NewOrm(dbAdapter)
-	o.Assert().NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
+	o.NoError(d3Orm.Register((*Book)(nil), (*Shop)(nil), (*ShopProfile)(nil), (*Author)(nil)))
 
 	session := d3Orm.CreateSession()
 
 	shop, err := createAndPersistsShop(d3Orm, session)
-	o.Assert().NoError(err)
+	o.NoError(err)
 
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
 	newProfile := &ShopProfile{Description: "new shop profile"}
 	shop.Profile = entity.NewWrapEntity(newProfile)
@@ -277,11 +274,11 @@ func (o *PersistsTS) TestInsertThenFullUpdate() {
 	oldBook.Authors.Remove(1)
 
 	dbAdapter.ResetCounters()
-	o.Assert().NoError(session.Flush())
+	o.NoError(session.Flush())
 
-	o.Assert().Equal(1, dbAdapter.DeleteCounter())
-	o.Assert().Equal(2, dbAdapter.UpdateCounter())
-	o.Assert().Equal(4, dbAdapter.InsertCounter())
+	o.Equal(1, dbAdapter.DeleteCounter())
+	o.Equal(2, dbAdapter.UpdateCounter())
+	o.Equal(4, dbAdapter.InsertCounter())
 
 	helpers.NewPgTester(o.T(), o.pgDb).
 		SeeOne("SELECT * FROM shop_p WHERE name = $1 and profile_id = $2", "new shop", newProfile.Id).
