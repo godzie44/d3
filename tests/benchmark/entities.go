ec82b6a4869b8c54bd10a4e5f4f387a06881b420
godzie44 <godzie@yandex.ru>
2020-06-03 01:16:25 +0300

    add benchmarks

---
+++ tests/benchmark/entities.go
@@ -0,0 +1,33 @@
+package benchmark
+
+import (
+	"d3/orm/entity"
+	"database/sql"
+)
+
+//d3:entity
+type shop struct {
+	ID      sql.NullInt32        `d3:"pk:auto"`
+	Books   entity.Collection    `d3:"one_to_many:<target_entity:d3/tests/benchmark/book,join_on:shop_id,delete:nullable>,type:eager"`
+	Profile entity.WrappedEntity `d3:"one_to_one:<target_entity:d3/tests/benchmark/profile,join_on:profile_id,delete:cascade>,type:eager"`
+	Name    string               `d3:"column:name"`
+}
+
+//d3:entity
+type profile struct {
+	Id          sql.NullInt32 `d3:"pk:auto"`
+	Description string
+}
+
+//d3:entity
+type book struct {
+	Id      sql.NullInt32     `d3:"pk:auto"`
+	Authors entity.Collection `d3:"many_to_many:<target_entity:d3/tests/benchmark/author,join_on:book_id,reference_on:author_id,join_table:book_author_m>,type:lazy"`
+	Name    string
+}
+
+//d3:entity
+type author struct {
+	Id   sql.NullInt32 `d3:"pk:auto"`
+	Name string
+}
